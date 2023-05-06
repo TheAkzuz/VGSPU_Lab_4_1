4c2c68c1c3e1f557ba2354f44273446d97d3b067
TheAkzuz <131234047+TheAkzuz@users.noreply.github.com>
2023-05-06 10:46:14 +0300

    Add files via upload

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Hello
+{
+class Program
+{
+static double min(double a, double b)
+{
+return (a < b) ? a : b;
+}
+static void Main(string[] args)
+{
+Console.Write("x=");
+double x = double.Parse(Console.ReadLine());
+Console.Write("y=");
+double y = double.Parse(Console.ReadLine());
+double z = min(3 * x, 2 * y) + min(x - y, x + y);
+Console.WriteLine("z=" + z);
+}
+}
+}
